5f72b72c2d9e5caca4ea2c1a6f76c34e1a8a0752
Tavet Takesian <indexviross@hotmail.com>
2017-09-12 12:29:02 +0200

    Opdracht 1 af

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -17,24 +17,10 @@ public class Main {
         //Create startTimer
         long startTime = System.currentTimeMillis();
         //Sort 800000 times
-        for (int i = 0; i < 1; i++) {
-            ArrayList<Integer> dupplicate = new ArrayList();
+        for (int i = 0; i < 800001; i++) {
             //Fill the array with random numbers 9 times
             for (int j = 0; j < 10; j++) {
                 int rand = random.nextInt(9)+1;
-                if(!dupplicate.contains(rand)){
-                    dupplicate.add(rand);
-                }else if(dupplicate.contains(rand)) {
-                    boolean contains = true;
-                    while (contains){
-                        rand = random.nextInt(9)+1;
-                        if(dupplicate.contains(rand)){
-                            contains = true;
-                        }else {
-                            contains = false;
-                        }
-                    }
-                }
                 ints.add(rand);
             }
             //Sort the array
@@ -46,19 +32,25 @@ public class Main {
         }
         //Count the time spent
         long duration = (System.currentTimeMillis() - startTime);
-        System.out.println("Duration: "+duration);
+        //From milliseconds to seconds
+        duration = duration/1000;
+        //Print out the duration of the whole process
+        System.out.println("Duration: "+duration+" seconds");
     }
 
     //Opdracht 1
     private ArrayList<Integer> insertionSort(ArrayList<Integer> listToSort) {
 
-        for(Integer person: listToSort){
+        //A counter that will help us identify, the byteValue of dupplicated numbers
+        int counterLoop = 0;
+        //Loop through the unsorted list
+        for(Integer number: listToSort){
 
             //Set the current string that we are comparing
-            int current = person;
+            int current = number;
 
             //A counter that will start from the previous value instead of the current value
-            int counter = person.byteValue()-1;
+            int counter = counterLoop-1;
 
             //Loop throught the previous elements to check if they are smaller or larger than our current string
             while (counter != -1) {
@@ -66,18 +58,20 @@ public class Main {
                 if(current <= listToSort.get(counter)){
                     //Create a temporary variable to hold the larger value
                     int temp = listToSort.get(counter);
-                    //The index position of the current value
-                    int currentIndex = person.byteValue();
                     //Swap values
                     listToSort.set(counter,current);
-                    listToSort.set(currentIndex,temp);
+                    listToSort.set(counterLoop,temp);
                     //Relocate the new position of our current string that we were comparing
                     current = listToSort.get(counter);
                 }
                 //Go back to the previous value
                 counter--;
             }
+            //Count the byteValue position
+            counterLoop++;
+
         }
+        //Return a sorted list
         return listToSort;
     }
 }
