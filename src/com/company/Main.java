7811628b6fb46c2a33bf47205f5f7c0e4521d3c0
swart\Govert <gdswa@outlook.com>
2017-09-13 16:54:57 +0200

    merge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class Main {
 
@@ -12,34 +14,82 @@ public class Main {
 
     //Opdracht 1
     private void run() {
-    //    opdracht1();
-    //    opdracht2(10);
-        opdracht3(40,2);
-
+      // opdracht1(5000);
+        //opdracht2(10);
+       opdracht3(40,3);
     }
 
+    private ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right) {
+        int indexLinks = 0;
+        int indexRechts = 0;
+        ArrayList<Integer> merge = new ArrayList<>();
+        while (merge.size() < (left.size() + right.size())) {
+            if (indexLinks != left.size() && indexRechts != right.size()) {
+                if (right.get(indexRechts) > left.get(indexLinks)) {
+                    if (merge.size() == 0) {
+                        merge.add(left.get(indexLinks));
+                        indexLinks++;
+                    } else {
+                        merge.add(merge.size(), left.get(indexLinks));
+                        indexLinks++;
+                    }
+                } else if (right.get(indexRechts) < left.get(indexLinks)) {
+                    if (merge.size() == 0) {
+                        merge.add(right.get(indexRechts));
+                        indexRechts++;
+                    } else {
+                        merge.add(merge.size(), right.get(indexRechts));
+                        indexRechts++;
+                    }
+                } else if (right.get(indexRechts) == left.get(indexLinks)) {
 
-    private void opdracht1() {
+                    if (merge.size() == 0) {
+                        merge.add(right.get(indexRechts));
+                        merge.add(left.get(indexLinks));
+                        indexRechts++;
+                        indexLinks++;
+                    } else {
+                        merge.add(merge.size(), right.get(indexRechts));
+                        merge.add(merge.size(), left.get(indexLinks));
+                        indexRechts++;
+                        indexLinks++;
+                    }
+
+                }
+
+            } else if (indexLinks == left.size() && indexRechts != right.size()) {
+                while (indexRechts < right.size()) {
+                    merge.add(merge.size(), right.get(indexRechts));
+                    indexRechts++;
+                }
+
+            } else if (indexLinks != left.size() && indexRechts == right.size()) {
+                while (indexLinks < left.size()) {
+                    merge.add(merge.size(), left.get(indexLinks));
+                    indexLinks++;
+                }
+
+            }
+
+        }
+        return merge;
+    }
+
+    private void opdracht1(int max) {
         //Create a new array
-        ArrayList<Integer> ints = new ArrayList<>();
-        //Create a new random
-        Random random = new Random();
+        ArrayList<Integer> ints = getRandomNumberArray(max);
         //Create startTimer
         long startTime = System.currentTimeMillis();
-        //Sort 800000 times
-        for (int i = 0; i < 800001; i++) {
-            //Fill the array with random numbers 9 times
-            for (int j = 0; j < 10; j++) {
-                int rand = random.nextInt(9) + 1;
-                ints.add(rand);
-            }
-            //Sort the array
-            insertionSort(ints);
-            //Print out sorted array and duration
-            System.out.printf("Sorted array: " + ints.toString() + "\n");
-            //Clear the list
-            ints.clear();
+        InsertionSort insertionSort = new InsertionSort(ints);
+        Thread thread = new Thread(insertionSort);
+        thread.start();
+        try {
+            thread.join();
         }
+        catch (InterruptedException ex){
+        }
+        //Print out sorted array and duration
+        System.out.printf("Sorted array: " + ints.toString() + "\n");
         //Count the time spent
         long duration = (System.currentTimeMillis() - startTime);
         //From milliseconds to seconds
@@ -49,28 +99,38 @@ public class Main {
     }
 
 
+
+    private ArrayList<Integer> getRandomNumberArray(int max){
+        ArrayList<Integer> row = new ArrayList<>();
+        //Create a new random
+        Random random = new Random();
+        int rand;
+        //Create a set that will hold the random numbers
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        //Loop max times and add the random generated numbers to our set
+        for (int j = 0; j < max; j++) {
+            rand = random.nextInt(max) + 1;
+            linkedHashSet.add(rand);
+        }
+        //Add the set to the arraylist for sorting
+        for (Integer integer : linkedHashSet){
+            row.add(integer);
+        }
+        return row;
+    }
     //Opdracht 2
     private void opdracht2(int arraySize) {
         //Create 3 arrays
         ArrayList<Integer> left = new ArrayList<>();
         ArrayList<Integer> right = new ArrayList<>();
-        ArrayList<Integer> row = new ArrayList<>();
-        //Create a new random
-        Random random = new Random();
-        //Create startTimer
-        long startTime = System.currentTimeMillis();
-        for (int i = 0; i < arraySize; i++) {
-            int rand = random.nextInt(9) + 1;
-            row.add(rand);
-        }
-        //opdracht2 the array in 2
+        ArrayList<Integer> row = getRandomNumberArray(arraySize);
+       //Split array
         for (int j = 0; j < arraySize / 2; j++) {
             left.add(row.get(j));
         }
         for (int j = arraySize / 2 + 1; j < arraySize; j++) {
             right.add(row.get(j));
         }
-
         InsertionSort sort1 = new InsertionSort(left);
         InsertionSort sort2 = new InsertionSort(right);
         Thread t1 = new Thread(sort1);
@@ -82,40 +142,28 @@ public class Main {
             t2.join();
         } catch (InterruptedException ex) {
         }
-
-
-        //Sort the array
-        insertionSort(row);
-
-
+        ArrayList<Integer> merged = merge(left,right);
         //Print out sorted array and duration
-        System.out.printf("Sorted array: " + row.toString() + "\n");
+        System.out.printf("Sorted array: " + merged.toString() + "\n");
         //Clear the list
         row.clear();
 
 
         //Count the time spent
-        long duration = (System.currentTimeMillis() - startTime);
+        //long duration = (System.currentTimeMillis() - startTime);
         //From milliseconds to seconds
-        duration = duration / 1000;
+        //duration = duration / 1000;
         //Print out the duration of the whole process
-        System.out.println("Duration: " + duration + " seconds");
+       // System.out.println("Duration: " + duration + " seconds");
     }
 
 
     //Opdracht 3
     private void opdracht3(int arraySize,int threshold){
         //Create an array
-        ArrayList<Integer> row = new ArrayList<>();
-        //Create a new random
-        Random random = new Random();
+        ArrayList<Integer> row = getRandomNumberArray(arraySize);
         //Create startTimer
         long startTime = System.currentTimeMillis();
-        //fill Array with random numbers
-        for (int i = 0; i < arraySize; i++) {
-            int rand = random.nextInt(9) + 1;
-            row.add(rand);
-        }
         InsertionSortSplitter insertionSortSplitter = new InsertionSortSplitter(row,threshold);
         Thread run = new Thread(insertionSortSplitter);
         run.start();
@@ -126,56 +174,19 @@ public class Main {
         row = row;
     }
 
-    //Opdracht 1
-    private ArrayList<Integer> insertionSort(ArrayList<Integer> listToSort) {
 
-        //Loop through the unsorted list
-        for (Integer number : listToSort) {
-
-            //Set the current string that we are comparing
-            int current = number;
-
-            //A counter that will start from the previous value instead of the current value
-            int counter = listToSort.indexOf(number) - 1;
-
-            //Loop throught the previous elements to check if they are smaller or larger than our current string
-            while (counter != -1) {
-                //If the current string is smaller than its previouse string, then swap their values
-                if (current <= listToSort.get(counter)) {
-                    //Create a temporary variable to hold the larger value
-                    int temp = listToSort.get(counter);
-                    //The index position of the current value
-                    int currentIndex = listToSort.indexOf(number);
-                    //Swap values
-                    listToSort.set(counter, current);
-                    listToSort.set(currentIndex, temp);
-                    //Relocate the new position of our current string that we were comparing
-                    current = listToSort.get(counter);
-                }
-                //Go back to the previous value
-                counter--;
-            }
-
-        }
-        //Return a sorted list
-        return listToSort;
-    }
 
     public class InsertionSort implements Runnable {
 
         private ArrayList<Integer> listToSort;
 
         public void run() {
-            //A counter that will help us identify, the byteValue of dupplicated numbers
-            int counterLoop = 0;
             //Loop through the unsorted list
             for (Integer number : listToSort) {
-
                 //Set the current string that we are comparing
                 int current = number;
-
                 //A counter that will start from the previous value instead of the current value
-                int counter = counterLoop - 1;
+                int counter = listToSort.indexOf(number) - 1;
 
                 //Loop throught the previous elements to check if they are smaller or larger than our current string
                 while (counter != -1) {
@@ -183,21 +194,19 @@ public class Main {
                     if (current <= listToSort.get(counter)) {
                         //Create a temporary variable to hold the larger value
                         int temp = listToSort.get(counter);
+                        //The index position of the current value
+                        int currentIndex = listToSort.indexOf(number);
                         //Swap values
                         listToSort.set(counter, current);
-                        listToSort.set(counterLoop, temp);
+                        listToSort.set(currentIndex, temp);
                         //Relocate the new position of our current string that we were comparing
                         current = listToSort.get(counter);
                     }
                     //Go back to the previous value
                     counter--;
                 }
-                //Count the byteValue position
-                counterLoop++;
-            }
-            listToSort = listToSort;
-            int a = 0;
 
+            }
         }
 
         public InsertionSort(ArrayList<Integer> listToSort) {
@@ -298,10 +307,18 @@ public class Main {
                     t2.join();
                 } catch (InterruptedException ex) {
                 }
+                merge(left,right);
+
             }else{
                 InsertionSort s = new InsertionSort(listToSort);
                 Thread run = new Thread(s);
                 run.start();
+                try {
+                    run.join();
+                } catch (InterruptedException ex) {
+                }
+
+
             }
         }
 
