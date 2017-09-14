ab46fb8f0821fc8d9f6b2d3656968789f1fbf5e4
Tavet Takesian <indexviross@hotmail.com>
2017-09-14 13:42:32 +0200

    Comments added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,13 +12,135 @@ public class Main {
     }
 
 
-    //Opdracht 1
+    /***
+     * This method will run all of the written exercises
+     */
     private void run() {
-        opdracht1(1000);
-        opdracht2(40);
-        opdracht3(1000000, 100);
+       opdracht1(1000);
+       opdracht2(40);
+       opdracht3(800000, 100);
     }
 
+
+    /***
+     * A function that will use the getRandomNumberArray function to create an unsorted arraylist.
+     * The unsorted arraylist will be sorted using the insertion sort. A timer will start when starting
+     * the sorting and will finish counting when the sorting is finished. When finished, the sorted array
+     * will print out + the duration in seconds
+     * @param max The length that will be given to the arraylist
+     */
+    private void opdracht1(int max) {
+        //Create a new array
+        ArrayList<Integer> ints = getRandomNumberArray(max);
+        //Create startTimer
+        long startTime = System.currentTimeMillis();
+        InsertionSort insertionSort = new InsertionSort(ints);
+        Thread thread = new Thread(insertionSort);
+        thread.start();
+        try {
+            thread.join();
+        } catch (InterruptedException ex) {
+        }
+        //Print out sorted array and duration
+        System.out.printf("Sorted array: " + ints.toString() + "\n");
+        //Count the time spent
+        long duration = (System.currentTimeMillis() - startTime);
+        //From milliseconds to seconds
+        duration = duration / 1000;
+        //Print out the duration of the whole process
+        System.out.println("Duration: " + duration + " seconds");
+    }
+
+
+    /***
+     * This function will create a random numbers array using the getRandomNumberArray function
+     * and then split the given array into 2 and run each in a thread.
+     * After the sorting, the splitted arrays will me merged using the merge function.
+     * @param arraySize The length of the array
+     */
+    private void opdracht2(int arraySize) {
+        //Create 3 arrays
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+        ArrayList<Integer> row = getRandomNumberArray(arraySize);
+        //Split array
+        for (int j = 0; j < row.size() / 2; j++) {
+            left.add(row.get(j));
+        }
+        for (int j = arraySize / 2 + 1; j < row.size(); j++) {
+            right.add(row.get(j));
+        }
+        InsertionSort sort1 = new InsertionSort(left);
+        InsertionSort sort2 = new InsertionSort(right);
+        Thread t1 = new Thread(sort1);
+        Thread t2 = new Thread(sort2);
+        t1.start();
+        t2.start();
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException ex) {
+        }
+        ArrayList<Integer> merged = new ArrayList<>();
+        merged.addAll(left);
+        merged.addAll(right);
+        InsertionSort finalSort = new InsertionSort(merged);
+        Thread finalThead = new Thread(finalSort);
+        finalThead.run();
+        try{
+            finalThead.join();
+        }
+        catch (InterruptedException ex){
+        }
+
+        //Print out sorted array and duration
+        System.out.printf("Sorted array: " + merged.toString() + "\n");
+        //Clear the list
+        row.clear();
+    }
+
+
+    /***
+     * This function will create a random numbers array using the getRandomNumberArray function
+     * and then split the given array until each thread holds an array that has a length of the threshhold.
+     * Each thread will sort the array that it contains using the insertionSort function.
+     * After the sorting, the splitted arrays will me merged using the merge function.
+     * @param arraySize The length of the array
+     * @param threshold The length of the array which each thread will contain
+     */
+    private void opdracht3(int arraySize, int threshold) {
+        //Create an array
+        ArrayList<Integer> row = getRandomNumberArray(arraySize);
+        //Create startTimer
+        long startTime = System.currentTimeMillis();
+        InsertionSortSplitter insertionSortSplitter = new InsertionSortSplitter(row, threshold);
+        Thread run = new Thread(insertionSortSplitter);
+        run.start();
+        try {
+            run.join();
+        } catch (InterruptedException ex) {
+        }
+        row = insertionSortSplitter.getListToSort();
+        System.out.printf("Done\n");
+        //Count the time spent
+        long duration = (System.currentTimeMillis() - startTime);
+        //From milliseconds to seconds
+        duration = duration / 1000;
+        //Print out the duration of the whole process
+        System.out.println("Duration: " + duration + " seconds");
+
+
+    }
+
+    /***
+     * This function will merge the incoming arrays together. When the user runs this function, the program
+     * will take the left arraylist and the right arraylist and compare them to each other until they are merged
+     * in the correct order. listToSort arraylist is used to compare the left and the right arraylist with it.
+     * @param left The left arraylist
+     * @param right The right arraylist
+     * @param listToSort The original list that will be compared to
+     * @return Returns a merged array that is sorted
+     */
     private ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right,ArrayList<Integer> listToSort) {
         int leftIndex = 0;
         int rightIndex = 0;
@@ -58,124 +180,12 @@ public class Main {
         return listToSort;
     }
 
-    private void opdracht1(int max) {
-        //Create a new array
-        ArrayList<Integer> ints = getRandomNumberArray(max);
-        //Create startTimer
-        long startTime = System.currentTimeMillis();
-        InsertionSort insertionSort = new InsertionSort(ints);
-        Thread thread = new Thread(insertionSort);
-        thread.start();
-        try {
-            thread.join();
-        } catch (InterruptedException ex) {
-        }
-        //Print out sorted array and duration
-        System.out.printf("Sorted array: " + ints.toString() + "\n");
-        //Count the time spent
-        long duration = (System.currentTimeMillis() - startTime);
-        //From milliseconds to seconds
-        duration = duration / 1000;
-        //Print out the duration of the whole process
-        System.out.println("Duration: " + duration + " seconds");
-    }
-
-
-    private ArrayList<Integer> getRandomNumberArray(int max) {
-        ArrayList<Integer> row = new ArrayList<>();
-        //Create a new random
-        Random random = new Random();
-        int rand;
-        //Create a set that will hold the random numbers
-        Set<Integer> linkedHashSet = new LinkedHashSet<>();
-        //Loop max times and add the random generated numbers to our set
-        for (int j = 0; j < max; j++) {
-            rand = random.nextInt(max) + 1;
-            boolean added = linkedHashSet.contains(rand);
-            if(added){
-                j--;
-            }else {
-                linkedHashSet.add(rand);
-            }
-        }
-        //Add the set to the arraylist for sorting
-        for (Integer integer : linkedHashSet) {
-            row.add(integer);
-        }
-        return row;
-    }
-
-    //Opdracht 2
-    private void opdracht2(int arraySize) {
-        //Create 3 arrays
-        ArrayList<Integer> left = new ArrayList<>();
-        ArrayList<Integer> right = new ArrayList<>();
-        ArrayList<Integer> row = getRandomNumberArray(arraySize);
-        //Split array
-        for (int j = 0; j < row.size() / 2; j++) {
-            left.add(row.get(j));
-        }
-        for (int j = arraySize / 2 + 1; j < row.size(); j++) {
-            right.add(row.get(j));
-        }
-        InsertionSort sort1 = new InsertionSort(left);
-        InsertionSort sort2 = new InsertionSort(right);
-        Thread t1 = new Thread(sort1);
-        Thread t2 = new Thread(sort2);
-        t1.start();
-        t2.start();
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException ex) {
-        }
-        ArrayList<Integer> merged = new ArrayList<>();
-        merged.addAll(left);
-        merged.addAll(right);
-        InsertionSort finalSort = new InsertionSort(merged);
-        Thread finalThead = new Thread(finalSort);
-        finalThead.run();
-        try{
-            finalThead.join();
-        }
-        catch (InterruptedException ex){
-        }
-
-
-        //Print out sorted array and duration
-        System.out.printf("Sorted array: " + merged.toString() + "\n");
-        //Clear the list
-        row.clear();
-
-
-        //Count the time spent
-        //long duration = (System.currentTimeMillis() - startTime);
-        //From milliseconds to seconds
-        //duration = duration / 1000;
-        //Print out the duration of the whole process
-        // System.out.println("Duration: " + duration + " seconds");
-    }
-
-
-    //Opdracht 3
-    private void opdracht3(int arraySize, int threshold) {
-        //Create an array
-        ArrayList<Integer> row = getRandomNumberArray(arraySize);
-        //Create startTimer
-        long startTime = System.currentTimeMillis();
-        InsertionSortSplitter insertionSortSplitter = new InsertionSortSplitter(row, threshold);
-        Thread run = new Thread(insertionSortSplitter);
-        run.start();
-        try {
-            run.join();
-        } catch (InterruptedException ex) {
-        }
-        row = insertionSortSplitter.getListToSort();
-        System.out.printf("Done");
-
-    }
-
-
+    /***
+     * This class provides the insertion sort for the unsorted arraylist that run through it
+     * this class implements Runnable so that it is able to be run in a thread. When the function run()
+     * is used, the program will run through the ararylist and compare the numbers to sort them from low
+     * to high.
+     */
     public class InsertionSort implements Runnable {
 
         private ArrayList<Integer> listToSort;
@@ -213,62 +223,6 @@ public class Main {
             this.listToSort = listToSort;
         }
 
-        private ArrayList<Integer> merge(ArrayList<Integer> left, ArrayList<Integer> right,ArrayList<Integer> listToSort) {
-            int indexLinks = 0;
-            int indexRechts = 0;
-            while (listToSort.size() < (left.size() + right.size())) {
-                if (indexLinks != left.size() && indexRechts != right.size()) {
-                    if (right.get(indexRechts) > left.get(indexLinks)) {
-                        if (listToSort.size() == 0) {
-                            listToSort.add(left.get(indexLinks));
-                            indexLinks++;
-                        } else {
-                            listToSort.add(listToSort.size(), left.get(indexLinks));
-                            indexLinks++;
-                        }
-                    } else if (right.get(indexRechts) < left.get(indexLinks)) {
-                        if (listToSort.size() == 0) {
-                            listToSort.add(right.get(indexRechts));
-                            indexRechts++;
-                        } else {
-                            listToSort.add(listToSort.size(), right.get(indexRechts));
-                            indexRechts++;
-                        }
-                    } else if (right.get(indexRechts) == left.get(indexLinks)) {
-
-                        if (listToSort.size() == 0) {
-                            listToSort.add(right.get(indexRechts));
-                            listToSort.add(left.get(indexLinks));
-                            indexRechts++;
-                            indexLinks++;
-                        } else {
-                            listToSort.add(listToSort.size(), right.get(indexRechts));
-                            listToSort.add(listToSort.size(), left.get(indexLinks));
-                            indexRechts++;
-                            indexLinks++;
-                        }
-
-                    }
-
-                } else if (indexLinks == left.size() && indexRechts != right.size()) {
-                    while (indexRechts < right.size()) {
-                        listToSort.add(listToSort.size(), right.get(indexRechts));
-                        indexRechts++;
-                    }
-
-                } else if (indexLinks != left.size() && indexRechts == right.size()) {
-                    while (indexLinks < left.size()) {
-                        listToSort.add(listToSort.size(), left.get(indexLinks));
-                        indexLinks++;
-                    }
-
-                }
-
-            }
-            return listToSort;
-        }
-
-
         public void main(String args[]) {
             new Main().run();
         }
@@ -276,6 +230,14 @@ public class Main {
 
     }
 
+    /***
+     * This class can be used to split the incoming array into threads. When the run function is called,
+     * the program will split the given array into the provided threshhold. If the length of the array is
+     * 100000 and the threshhold is 100, then the array will be splitted until each thread contains an array
+     * with the length of 100. After the splitting, the program starts sorting in threads using the insertionSort.
+     * After finishing with the sorting, the function rus the merge method to merge the splited arrays in to a
+     * complete sorted array.
+     */
     public class InsertionSortSplitter implements Runnable {
 
         private ArrayList<Integer> listToSort;
@@ -333,6 +295,36 @@ public class Main {
             new Main().run();
         }
 
+    }
+
+    /***
+     * A function that will create an array with the length that is inputed with max.
+     * A linkedHashSet is used to prevent dupplication
+     * @param max the length of the arraylist
+     * @return an arraylist filled with random,unsorted, non-dupplicate numbers
+     */
+    private ArrayList<Integer> getRandomNumberArray(int max) {
+        ArrayList<Integer> row = new ArrayList<>();
+        //Create a new random
+        Random random = new Random();
+        int rand;
+        //Create a set that will hold the random numbers
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        //Loop max times and add the random generated numbers to our set
+        for (int j = 0; j < max; j++) {
+            rand = random.nextInt(max) + 1;
+            boolean added = linkedHashSet.contains(rand);
+            if(added){
+                j--;
+            }else {
+                linkedHashSet.add(rand);
+            }
+        }
+        //Add the set to the arraylist for sorting
+        for (Integer integer : linkedHashSet) {
+            row.add(integer);
+        }
+        return row;
     }
 }
 
